571b890446f4588b98eebe1953f64bf131b66300
Kinda Lam <lamkinun@gmail.com>
2021-11-24 19:07:03 +0800

    cnn

---
+++ 1_Basic/cnn_demo.py
@@ -0,0 +1,34 @@
+import torch
+from torchvision.models import vgg16
+import cv2
+import os
+
+os.environ['TORCH_HOME'] = '.'
+dev = "cuda" if torch.cuda.is_available() else "cpu"
+
+model = vgg16(pretrained=True)
+model.eval()
+model.to(dev)
+with open("imagenet_classes.txt", "r") as f:
+    classes = [x.strip() for x in f.readlines()]
+
+camera = cv2.VideoCapture(0)
+while camera.isOpened():
+    success, frame = camera.read()
+    if not success: break
+
+    image = torch.as_tensor(frame, dtype=torch.float32)
+    image = image / 255
+    image = torch.permute(image, (2, 0, 1))
+    image = torch.unsqueeze(image, dim=0)
+    image = image.to(dev)
+    out = model(image)
+    idx = torch.max(out, dim=1).indices
+    print(classes[idx])
+
+    cv2.imshow("frame", frame)
+    key_code = cv2.waitKey(1)
+    if key_code in [27, ord('q')]:
+        break
+camera.release()
+cv2.destroyAllWindows()
