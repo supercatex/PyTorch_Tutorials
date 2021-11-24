986e9b792d7dd7a9460c30d6ec9078e83214b5e9
Kinda Lam <lamkinun@gmail.com>
2021-11-24 20:28:31 +0800

    rcnn

--- 1_Basic/rcnn_demo.py
+++ 1_Basic/rcnn_demo.py
@@ -1,15 +1,15 @@
 import torch
-from torchvision.models import vgg16
-import cv2
+from torchvision.models.detection import fasterrcnn_resnet50_fpn
 import os
+import cv2
 
 os.environ['TORCH_HOME'] = '.'
 dev = "cuda" if torch.cuda.is_available() else "cpu"
 
-model = vgg16(pretrained=True)
+model = fasterrcnn_resnet50_fpn(pretrained=True)
 model.eval()
 model.to(dev)
-with open("imagenet_classes.txt", "r") as f:
+with open("coco_classes.txt", "r") as f:
     classes = [x.strip() for x in f.readlines()]
 
 camera = cv2.VideoCapture(0)
@@ -24,9 +24,15 @@ while camera.isOpened():
     image = torch.unsqueeze(image, dim=0)
     image = image.to(dev)
     out = model(image)
-    idx = torch.max(out, dim=1).indices
-    print(classes[idx])
-
+    o_boxes = out[0]["boxes"]
+    o_labels = out[0]["labels"]
+    o_scores = out[0]["scores"]
+    for box, label, score in zip(o_boxes, o_labels, o_scores):
+        if score < 0.7: continue
+        x1, y1, x2, y2 = map(int, box)
+        color = (0, 255, 0)
+        cv2.rectangle(frame, (x1, y1), (x2, y2), color, 2)
+        print(label, classes[label - 1])
     cv2.imshow("frame", frame)
     key_code = cv2.waitKey(1)
     if key_code in [27, ord('q')]:
